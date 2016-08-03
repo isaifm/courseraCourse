c743ddabc9cffe0247c219502d872242dda8a28e
isaifm <isaif@mail.usf.edu>
2016-08-03 16:34:41 -0700

    Script2

---
+++ module4-solution/script2.js
@@ -0,0 +1,14 @@
+(function (window) {
+  var byeSpeaker = {};
+  //johnGreeter.name = "John";
+  var speakWord = "Bye ";
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+
+
